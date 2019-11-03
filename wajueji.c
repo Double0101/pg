f1cc05e527858111511a7f9a45cd5c98986d6063
Double <double0101@foxmail.com>
2019-11-04 00:23:09 +0800

    see you tomorrow

---
+++ wajueji.c
@@ -0,0 +1,16 @@
+//
+// Created by Double on 2019-11-03.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+    int count, school = 100000, score = 0;
+    int sumSores[100001];
+    scanf("%d", &count);
+    while (count--) {
+
+    }
+}
