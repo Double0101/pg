5f7fb679f22fc0b02d13a5d6d7dde85f096f1b84
Double0101 <double0101@foxmail.com>
2019-11-04 22:08:15 +0800

    pat b1032

--- wajueji.c
+++ wajueji.c
@@ -7,10 +7,19 @@
 
 int main()
 {
-    int count, school = 100000, score = 0;
-    int sumSores[100001];
+    int count, school = 100001, score = 0;
+    int sumSores[100002] = {0};
+    sumSores[school] = 0;
     scanf("%d", &count);
     while (count--) {
-
+        int sno, ss;
+        scanf("%d %d", &sno, &ss);
+        sumSores[sno] += ss;
+        if (sumSores[sno] > score)
+        {
+            school = sno;
+            score = sumSores[sno];
+        }
     }
+    printf("%d %d\n", school, score);
 }
