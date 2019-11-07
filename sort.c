323b7249ee4481380fcecf0acf4d319dc7f533be
Double0101 <double0101@foxmail.com>
2019-11-07 22:15:16 +0800

    insert sort

--- sort.c
+++ sort.c
@@ -26,3 +26,23 @@ int selectSort()
 
     return 0;
 }
+
+int insertSort()
+{
+    int size = sizeof(A) / sizeof(A[0]);
+    int i, j;
+    for (i = 0; i < size; ++i) {
+        int a = A[i];
+        j = i - 1;
+        while (j >= 0 && A[j] > a) {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[++j] = a;
+    }
+
+    i = 0;
+    while (i < size) {
+        printf("%d ", A[i++]);
+    }
+}
