53553b58f471bbf998c6fb167c92632c6c82188c
Double0101 <double0101@foxmail.com>
2020-03-25 22:44:25 +0800

    lcm

--- gcdlcm.c
+++ gcdlcm.c
@@ -7,10 +7,16 @@ int gcd(int a, int b)
     return !b ? a : gcd(b, a%b);
 }
 
+int lcm(int a, int b)
+{
+    int c = gcd(a, b);
+    return a / c * b;
+}
+
 int main(){
     srand(time(NULL));
     int a = rand()%1000;
     int b = rand()%1000;
 
-    printf("%d %d gcd %d\n", a, b, gcd(a, b));
+    printf("%d %d gcd %d lcm %d\n", a, b, gcd(a, b), lcm(a, b));
 }
