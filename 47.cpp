493060512fb439eee8df73104c2ecef73f253dcf
Double0101 <double0101@foxmail.com>
2021-01-31 00:19:51 +0800

    day 12

--- 47.cpp
+++ 47.cpp
@@ -1,48 +1,34 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
-#include <string>
+#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
+char names[40010][5];
 bool cmp(int s1, int s2)
 {
-    return s1 < s2;
-}
-const int maxn = 26*26*26*10;
-string names[maxn];
-
-int getid(string s)
-{
-    int id = 0;
-    for (int i = 0; i < 3; ++i) {
-        id = id * 26 + s[i] - 'A';
-    }
-    id = id * 10 + s[3] - '0';
-    return id;
+    return strcmp(names[s1], names[s2]) < 0;
 }
 
 int main()
 {
     int sn, cn;
-    cin >> sn >> cn;
-    vector<int> namelist[cn];
+    scanf("%d %d", &sn, &cn);
+    vector<int> namelist[cn+1];
     for (int i = 0; i < sn; ++i) {
-        string name;
         int a, b;
-        cin >> name >> a;
-        int id = getid(name);
-        names[id] = name;
+        scanf("%s %d", names[i], &a);
         for (int j = 0; j < a; ++j) {
-            cin >> b;
-            namelist[b-1].push_back(id);
+            scanf("%d", &b);
+            namelist[b].push_back(i);
         }
     }
-    for (int i = 0; i < cn; ++i) {
-        cout << i+1 << " " << namelist[i].size() << endl;
+    for (int i = 1; i <= cn; ++i) {
+        printf("%d %lu\n", i, namelist[i].size());
         sort(namelist[i].begin(), namelist[i].end(), cmp);
         for (int s : namelist[i]) {
-            cout << names[s] << endl;
+            printf("%s\n", names[s]);
         }
     }
     return 0;
